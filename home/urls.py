e3bc213cb688cd8537647302a4bf258323e41a3d
Jayalekshmi K S <jayalekshmiks112@gmail.com>
2023-04-16 19:34:44 +0530

    Home modified

--- home/urls.py
+++ home/urls.py
@@ -1,27 +1,8 @@
-"""
 from django.urls import path
-#from .views import upload_file, delete_file
-
-from . import views
-app_name='home'
+from .views import UploadFileView, DeleteFileView, RetrieveFileView
 
 urlpatterns = [
-    #path('',views.home, name='home'),
-    path('upload/', views.upload_file.as_view(), name='upload'),
-    path('delete/<int:file_id>/', views.delete_file.as_view(), name='delete'),
-]
-"""
-from django.urls import path
-from django.conf.urls import url, include
-from rest_framework import routers
-from .views import FileViewSet, home, upload_file, delete_file
-
-router = routers.DefaultRouter()
-router.register(r'files', FileViewSet)
-
-urlpatterns = [
-    path('', home, name='home'),
-    path('upload/', upload_file, name='upload'),
-    path('delete/<int:file_id>/', delete_file, name='delete'),
-    url(r'^api/', include(router.urls)),
+    path('upload/', UploadFileView.as_view(), name='upload'),
+    path('delete/<int:id>/', DeleteFileView.as_view(), name='delete'),
+    path('retrieve/', RetrieveFileView.as_view(), name='retrieve'),
 ]
