4364a41571485bb91d1115d64651fcbefc35a53d
Nandakumar1381 <76779019+Nandakumar1381@users.noreply.github.com>
2023-04-19 01:39:58 +0530

    Update settings.py

--- cloud/settings.py
+++ cloud/settings.py
@@ -119,7 +119,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-CORS_ALLOW_ALL_ORIGINS = True
+CSRF_TRUSTED_ORIGINS = ["https://*.ngrok-free.app"]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
